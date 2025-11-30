3089eb4f84d0c9f829d13e60a059d82f73c6fe9e
root <root@wien1.exploro.one>
2025-11-30 13:05:16 +0000

    start working on web

---
+++ web/globalping/app/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { Box, Card, Typography, CardContent } from "@mui/material";
+
+export default function Home() {
+  return (
+    <Box>
+      <Card>
+        <CardContent>
+          <Typography variant="h6">Global Ping</Typography>
+        </CardContent>
+      </Card>
+    </Box>
+  );
+}
