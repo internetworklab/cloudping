e6c23aecae940b4329c97a764426618ef8213ee3
root <root@wien1.exploro.one>
2026-01-05 05:36:25 +0000

    save

--- cmd/tcping/main.go
+++ cmd/tcping/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/binary"
 	"fmt"
 	"log"
 	"net"
@@ -11,6 +13,7 @@ import (
 	"time"
 
 	pkgutils "example.com/rbmq-demo/pkg/utils"
+	"github.com/google/btree"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/vishvananda/netlink"
@@ -36,6 +39,12 @@ func getPackets(rawConn *ipv4.RawConn) <-chan *PacketInfo {
 			if err != nil {
 				log.Printf("failed to read from raw connection: %v", err)
 				return
+			}
+			if hdr.Version != ipv4.Version {
+				continue
+			}
+			if hdr.Protocol != int(layers.IPProtocolTCP) {
+				continue
 			}
 			pktInfo := new(PacketInfo)
 			pktInfo.Hdr = hdr
@@ -91,22 +100,193 @@ func filterPackets(rbCh <-chan *PacketInfo) <-chan *PacketInfo {
 	return filteredCh
 }
 
-type Tracker struct{}
+type TrackEntry struct {
+	Key   []byte
+	Value *TCPSYNSentReceipt
+}
 
-func NewTracker() *Tracker {
-	return &Tracker{}
+func (tent *TrackEntry) Less(other btree.Item) bool {
+	otherEntry, ok := other.(*TrackEntry)
+	if !ok {
+		panic("other is not a TrackEntry")
+	}
+
+	if len(tent.Key) != len(otherEntry.Key) {
+		panic("keys are not of the same length")
+	}
+
+	return bytes.Compare(tent.Key, otherEntry.Key) < 0
+}
+
+type ServiceRequest struct {
+	Fn func(ctx context.Context) error
+	// Result channel is provided by the caller
+	Result chan error
+}
+
+type TrackerEventType string
+
+const (
+	TrackerEVTimeout  TrackerEventType = "timeout"
+	TrackerEVReceived TrackerEventType = "received"
+)
+
+type TrackerEvent struct {
+	Type  TrackerEventType
+	Entry *TrackEntry
+}
+
+type Tracker struct {
+	serviceChan chan chan ServiceRequest
+	EventC      chan TrackerEvent
+	store       *btree.BTree
+}
+
+type TrackerConfig struct {
+	EVBufferSize *int
+}
+
+func NewTracker(config *TrackerConfig) *Tracker {
+	tracker := new(Tracker)
+	tracker.serviceChan = make(chan chan ServiceRequest)
+	tracker.store = btree.New(2)
+	tracker.EventC = make(chan TrackerEvent)
+	if config != nil {
+		if config.EVBufferSize != nil {
+			tracker.EventC = make(chan TrackerEvent, *config.EVBufferSize)
+		}
+	}
+	return tracker
 }
 
 func (tk *Tracker) Run(ctx context.Context) {
-	// todo
+	go func() {
+		defer close(tk.serviceChan)
+		defer close(tk.EventC)
+
+		for {
+			requestCh := make(chan ServiceRequest)
+			select {
+			case <-ctx.Done():
+				return
+			case tk.serviceChan <- requestCh:
+				request := <-requestCh
+				request.Result <- request.Fn(ctx)
+			}
+		}
+	}()
+}
+
+func encodePort(port int) []byte {
+	if port < 0 || port > 65535 {
+		panic("port is out of range")
+	}
+
+	portBytes := make([]byte, 2)
+	binary.BigEndian.PutUint16(portBytes, uint16(port))
+	return portBytes
+}
+
+func buildKey(srcIP net.IP, srcPort int, dstIP net.IP, dstPort int) []byte {
+	key := make([]byte, 0)
+	key = append(key, srcIP...)
+	key = append(key, encodePort(srcPort)...)
+	key = append(key, dstIP...)
+	key = append(key, encodePort(dstPort)...)
+	return key
+}
+
+func (tk *Tracker) handleTimeout(ent *TrackEntry) {
+	key := ent.Key
+	requestCh, ok := <-tk.serviceChan
+	if !ok {
+		log.Printf("tracker is closed")
+		return
+	}
+
+	request := ServiceRequest{
+		Result: make(chan error),
+		Fn: func(ctx context.Context) error {
+			tk.EventC <- TrackerEvent{Type: TrackerEVTimeout, Entry: ent}
+			tk.store.Delete(&TrackEntry{Key: key})
+			return nil
+		},
+	}
+	requestCh <- request
+	if err := <-request.Result; err != nil {
+		log.Printf("failed to untrack: %v", err)
+	}
+
 }
 
 func (tk *Tracker) MarkSent(sentReceipt *TCPSYNSentReceipt) {
-	// todo
+	key := buildKey(sentReceipt.SrcIP, sentReceipt.SrcPort, sentReceipt.Request.DstIP, sentReceipt.Request.DstPort)
+	ent := &TrackEntry{Key: key, Value: sentReceipt}
+
+	requestCh, ok := <-tk.serviceChan
+	if !ok {
+		log.Printf("tracker is closed")
+		return
+	}
+
+	request := ServiceRequest{
+		Result: make(chan error),
+		Fn: func(ctx context.Context) error {
+			tk.store.ReplaceOrInsert(ent)
+			go func() {
+				for range ent.Value.TimeoutC {
+					tk.handleTimeout(ent)
+				}
+			}()
+			return nil
+		},
+	}
+	requestCh <- request
+	if err := <-request.Result; err != nil {
+		log.Printf("failed to mark sent: %v", err)
+	}
 }
 
 func (tk *Tracker) MarkReceived(receivedPkt *PacketInfo) {
-	// todo
+	if receivedPkt == nil || receivedPkt.Hdr == nil || receivedPkt.TCP == nil {
+		log.Printf("received packet is nil, or some inner headers are nil")
+		return
+	}
+	requestCh, ok := <-tk.serviceChan
+	if !ok {
+		log.Printf("tracker is closed")
+		return
+	}
+	key := buildKey(receivedPkt.Hdr.Dst, int(receivedPkt.TCP.DstPort), receivedPkt.Hdr.Src, int(receivedPkt.TCP.SrcPort))
+	receivedAt := time.Now()
+
+	request := ServiceRequest{
+		Fn: func(ctx context.Context) error {
+
+			item := tk.store.Get(&TrackEntry{Key: key})
+			if item == nil {
+				log.Printf("no entry found for key: %v", key)
+				return nil
+			}
+
+			ent, ok := item.(*TrackEntry)
+			if !ok {
+				panic("item is not a *TrackEntry")
+			}
+
+			ent.Value.ReceivedAt = receivedAt
+			ent.Value.ReceivedPkt = receivedPkt
+			ent.Value.ReceivedC <- receivedPkt
+			tk.store.Delete(&TrackEntry{Key: key})
+
+			return nil
+		},
+		Result: make(chan error),
+	}
+	requestCh <- request
+	if err := <-request.Result; err != nil {
+		log.Printf("failed to mark received: %v", err)
+	}
 }
 
 type TCPSYNSentReceipt struct {
@@ -114,9 +294,9 @@ type TCPSYNSentReceipt struct {
 	SrcPort     int
 	Request     *TCPSYNRequest
 	SentAt      time.Time
-	TimeoutC    chan time.Time
 	ReceivedAt  time.Time
 	ReceivedPkt *PacketInfo
+	TimeoutC    chan time.Time
 	ReceivedC   chan *PacketInfo
 }
 
@@ -151,9 +331,11 @@ func getSrcIP(dstIP net.IP) (net.IP, error) {
 	return routes[0].Src, nil
 }
 
-func (sender *TCPSYNSender) Send(rawConn *ipv4.RawConn, request *TCPSYNRequest) (*TCPSYNSentReceipt, error) {
+func (sender *TCPSYNSender) Send(rawConn *ipv4.RawConn, request *TCPSYNRequest, tracker *Tracker) (*TCPSYNSentReceipt, error) {
 	receipt := new(TCPSYNSentReceipt)
-	receipt.TimeoutC = make(chan time.Time)
+	receipt.TimeoutC = make(chan time.Time, 1)
+	receipt.ReceivedC = make(chan *PacketInfo, 1)
+
 	dstIP := request.DstIP
 	srcIP, err := getSrcIP(dstIP)
 	if err != nil {
@@ -213,27 +395,25 @@ func (sender *TCPSYNSender) Send(rawConn *ipv4.RawConn, request *TCPSYNRequest) 
 		Dst:      dstIP,
 		Flags:    ipv4.HeaderFlags(flags),
 	}
+
+	tracker.MarkSent(receipt)
+
 	err = rawConn.WriteTo(hdr, wb, nil)
 	if err != nil {
 		return nil, fmt.Errorf("failed to write to raw connection: %v", err)
 	}
+	receipt.SentAt = time.Now()
+	timer := time.NewTimer(request.Timeout)
 
 	go func() {
 		defer tcpListener.Close()
-
-		receipt.SentAt = time.Now()
-		timer := time.NewTimer(request.Timeout)
 		defer timer.Stop()
 
 		select {
 		case time := <-timer.C:
 			receipt.TimeoutC <- time
-		case pktInfo, ok := <-receipt.ReceivedC:
-			if !ok {
-				return
-			}
-			receipt.ReceivedAt = time.Now()
-			receipt.ReceivedPkt = pktInfo
+		case <-receipt.ReceivedC:
+			return
 		}
 	}()
 	return receipt, nil
@@ -279,8 +459,30 @@ func main() {
 	defer cancel()
 
 	sender := &TCPSYNSender{}
-	tracker := NewTracker()
+
+	trackerConfig := &TrackerConfig{}
+	tracker := NewTracker(trackerConfig)
 	tracker.Run(ctx)
+
+	go func(ctx context.Context) {
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			case event, ok := <-tracker.EventC:
+				if !ok {
+					return
+				}
+
+				switch event.Type {
+				case TrackerEVTimeout:
+					log.Printf("timeout event received")
+				case TrackerEVReceived:
+					log.Printf("received event received")
+				}
+			}
+		}
+	}(ctx)
 
 	go func(ctx context.Context) {
 		for {
@@ -304,16 +506,15 @@ func main() {
 		}
 	}(ctx)
 
-	receipt, err := sender.Send(rawConn, &TCPSYNRequest{
+	synRequest := &TCPSYNRequest{
 		DstIP:   dstIP,
 		DstPort: dstPort,
 		Timeout: 3 * time.Second,
-	})
+	}
+	_, err = sender.Send(rawConn, synRequest, tracker)
 	if err != nil {
 		log.Fatalf("failed to send tcp syn: %v", err)
 	}
-
-	tracker.MarkSent(receipt)
 
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
