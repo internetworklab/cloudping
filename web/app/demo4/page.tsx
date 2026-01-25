9aceada999a9d5ec517b5053f4886c2aeb7a02f6
duststars <i@exploro.one>
2026-01-25 20:14:28 +0800

    modeling traceroute report

--- web/app/demo4/page.tsx
+++ web/app/demo4/page.tsx
@@ -9,256 +9,104 @@ import {
   IconButton,
   Tooltip,
 } from "@mui/material";
-import { Fragment, useEffect, useRef, useState } from "react";
+import { Fragment, useEffect, useMemo, useRef, useState } from "react";
 import SaveIcon from "@mui/icons-material/Save";
+import { Row, CanvasTable } from "@/components/canvastable";
 
-type LineDrawCtx = {
-  fillStyle?: CanvasPattern | string | CanvasGradient;
-  baseline?: CanvasTextBaseline;
-  font?: string;
-  y: number;
-  lineGap: number;
-  lastMetrics?: TextMetrics;
-  x: number;
-  maxWidth?: number;
-  maxRight?: number;
-  maxHeight?: number;
+export type TracerouteReportLocation = {
+  city?: string;
+  countryAlpha2?: string;
 };
 
-function drawLine(
-  lineCtx: LineDrawCtx,
-  ctx: CanvasRenderingContext2D,
-  line: string
-): LineDrawCtx {
-  if (lineCtx.baseline) {
-    ctx.textBaseline = lineCtx.baseline;
-  }
-  if (lineCtx.fillStyle) {
-    ctx.fillStyle = lineCtx.fillStyle;
-  }
-  if (lineCtx.font) {
-    ctx.font = lineCtx.font;
-  }
-  ctx.fillText(line, lineCtx.x, lineCtx.y);
-  const lineMs = ctx.measureText(line);
-  const prevMaxWidth = lineCtx.maxWidth ?? 0;
-  const maxWidth = Math.max(prevMaxWidth, lineMs.width);
-  const prevMaxRight = lineCtx.maxRight ?? 0;
-  const maxRight = Math.max(prevMaxRight, lineCtx.x + lineMs.width);
-  const prevMaxHeight = lineCtx.maxHeight ?? 0;
-  const newHeight =
-    lineCtx.y +
-    lineMs.fontBoundingBoxAscent +
-    lineMs.fontBoundingBoxDescent +
-    lineCtx.lineGap;
-  const maxHeight = Math.max(prevMaxHeight, newHeight);
-  console.log("[dbg] maxRight:", maxRight);
-  console.log("[dbg] maxHeight:", maxHeight);
-  return {
-    ...lineCtx,
-    y: newHeight,
-    lastMetrics: lineMs,
-    maxWidth,
-    maxRight,
-    maxHeight,
-  };
-}
+export type TracerouteReportISP = {
+  // name of the isp, like 'Hurricane Electric'
+  ispName: string;
 
-function drawCursor(
-  x: number,
-  y: number,
-  ctx: CanvasRenderingContext2D,
-  w: number,
-  h: number,
-  color: CanvasPattern | string | CanvasGradient
-): void {
-  const currentFill = ctx.fillStyle;
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, w, h);
-  ctx.fillStyle = currentFill;
-}
-
-type ColDrawCtx = {
-  lineDrawCtx: LineDrawCtx;
-  columnGap: number;
-  y0?: number;
+  // usually in the format like 'AS12345'
+  asn: string;
 };
 
-type Cell = {
-  content: string;
-  empty?: boolean;
+export type TracerouteReportSource = {
+  nodeName: string;
+  isp?: TracerouteReportISP;
+  loc?: TracerouteReportLocation;
 };
 
-type Col = Cell[];
+export type TracerouteReportMode = "tcp" | "icmp" | "udp";
 
-type Row = Cell[];
+export type TracerouteReportRTTStat = {
+  lastMs: number;
+  samples: number[];
+};
 
-function transposeTable(rows: Row[]): Col[] {
-  if (rows.length === 0) {
-    return [];
-  }
-  const nCols = rows[0].length;
-  const cols: Col[] = [];
-  for (let c = 0; c < nCols; c++) {
-    const col: Col = [];
-    for (let r = 0; r < rows.length; r++) {
-      if (rows[r].length >= c + 1) {
-        col.push(rows[r][c]);
-      } else {
-        col.push({ content: "", empty: true });
-      }
-    }
-    cols.push(col);
-  }
-  return cols;
+export type TracerouteReportTXRXStat = {
+  sent: number;
+  replies: number;
+};
+
+export type TracerouteReportPeer = {
+  // if this field is falsy, mark it with a '*' in the screen,
+  // and skip render all the following fields.
+  timeout?: boolean;
+  rdns?: string;
+  ip: string;
+  loc?: TracerouteReportLocation;
+  isp?: TracerouteReportISP;
+  rtt?: TracerouteReportRTTStat;
+  stat?: TracerouteReportTXRXStat;
+};
+
+export type TracerouteReportHop = {
+  // ttl of the sent packets, usually, when doing traceroute, start with ttl=1,
+  // then increment ttl one by one until the final target is reached.
+  ttl: number;
+
+  // if the middlebox router is doing something like ecmp, a hop could be various peers.
+  peers: TracerouteReportPeer[];
+};
+
+export type TracerouteReport = {
+  // when the report is generated
+  date: number;
+
+  // in case that a originating node is multi-homed/BGP
+  sources: TracerouteReport[];
+
+  // the domain or ip address of the target host
+  destination: string;
+
+  // type of l4 sending packets, for linux, traceroute use udp by default,
+  // for windows, icmp is used.
+  mode: TracerouteReportMode;
+
+  hops: TracerouteReportHop[];
+};
+
+export function renderTracerouteReport(report: TracerouteReport): {
+  preamble: Row[];
+  tabularData: Row[];
+} {
+  const preamble: Row[] = [];
+  const tabularData: Row[] = [];
+
+  return { preamble, tabularData };
 }
 
-function drawCol(
-  colCtx: ColDrawCtx,
-  ctx: CanvasRenderingContext2D,
-  column: Col
-): ColDrawCtx {
-  let newColCtx: ColDrawCtx = {
-    ...colCtx,
-    y0: colCtx.y0 ?? colCtx.lineDrawCtx.y,
-  };
-  newColCtx.lineDrawCtx = {
-    ...newColCtx.lineDrawCtx,
-    maxWidth: 0,
-    y: newColCtx.y0!,
-  };
+export default function Page() {
+  const [show, setShow] = useState(true);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  for (const cell of column) {
-    newColCtx.lineDrawCtx = drawLine(newColCtx.lineDrawCtx, ctx, cell.content);
-  }
-
-  newColCtx.lineDrawCtx = {
-    ...newColCtx.lineDrawCtx,
-    x:
-      newColCtx.lineDrawCtx.x +
-      (newColCtx.lineDrawCtx.maxWidth ?? 0) +
-      newColCtx.columnGap,
-    maxWidth: 0,
-  };
-
-  return newColCtx;
-}
-
-function carriageReturn(lineCtx: LineDrawCtx): LineDrawCtx {
-  return {
-    ...lineCtx,
-    x: 0,
-  };
-}
-
-function Window(props: { canvasRef: React.RefObject<HTMLCanvasElement> }) {
-  const [w, setW] = useState(0);
-  const [h, setH] = useState(0);
-  const [maxW, setMaxW] = useState(0);
-  const [maxH, setMaxH] = useState(0);
-
-  const dpi = window.devicePixelRatio;
-  const R = Math.max(w * dpi, maxW);
-  const H = Math.max(h * dpi, maxH);
-
-  const boxRef = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    const ele = boxRef.current;
-    if (!ele) {
-      return;
-    }
-
-    const rect = ele.getBoundingClientRect();
-    console.log("[dbg] rect:", rect);
-
-    const obs = new ResizeObserver((entries) => {
-      for (const ent of entries) {
-        if (ent.target === ele) {
-          console.log("[dbg] ResizeObserver entry:", ent);
-          const cr = ent.contentRect;
-          if (cr) {
-            setW(cr.width);
-            setH(cr.height);
-          }
-        }
-      }
-    });
-
-    obs.observe(ele);
-
-    return () => {
-      obs.unobserve(ele);
-    };
-  });
-
-  useEffect(() => {
-    const ele = boxRef.current;
-    if (!ele) {
-      return;
-    }
-
-    const canvasEle = ele.querySelector("canvas");
-    if (!canvasEle) {
-      return;
-    }
-
-    const dpi = window.devicePixelRatio;
-
-    // canvasEle.setAttribute("width", `${w * dpi}`);
-    // canvasEle.setAttribute("height", `${h * dpi}`);
-
-    const ctx = canvasEle.getContext("2d");
-    if (!ctx) {
-      return;
-    }
-
-    ctx.fillStyle = "#262626";
-    ctx.fillRect(0, 0, R, H);
-
-    const deltaX = 10 * dpi;
-    const deltaY = 10 * dpi;
-    const translateMatrix = [
-      [1, 0, deltaX],
-      [0, 1, deltaY],
-      [0, 0, 1],
-    ];
-    ctx.setTransform(
-      translateMatrix[0][0],
-      translateMatrix[1][0],
-      translateMatrix[0][1],
-      translateMatrix[1][1],
-      translateMatrix[0][2],
-      translateMatrix[1][2]
-    );
-
-    const lines: string[] = [
+  const preamble: Row[] = useMemo(() => {
+    return [
       `Date: ${new Date().toISOString()}`,
       "Source: Node NYC1, AS65001 SOMEISP, SomeCity US",
       "Destination: pingable.burble.dn42",
       "Mode: ICMP",
-    ];
-    let lineCtx: LineDrawCtx = {
-      fillStyle: "#fff",
-      baseline: "top",
-      font: `${16 * dpi}px sans-serif`,
-      y: 0,
-      lineGap: 6 * dpi,
-      x: 0,
-    };
+    ].map((line) => [{ content: line }]);
+  }, []);
 
-    for (const line of lines) {
-      lineCtx = drawLine(lineCtx, ctx, line);
-    }
-
-    // drawCursor(lineCtx, ctx, 15 * dpi, 20 * dpi, "#111");
-
-    let colCtx: ColDrawCtx = {
-      lineDrawCtx: lineCtx,
-      columnGap: 30 * dpi,
-    };
-
-    const rows: Row[] = [
+  const tabularData: Row[] = useMemo(() => {
+    return [
       [
         { content: "TTL" },
         { content: "Peers" },
@@ -469,78 +317,7 @@ function Window(props: { canvasRef: React.RefObject<HTMLCanvasElement> }) {
         { content: "8 sent, 7 replies, 12.5 loss" },
       ],
     ];
-    const cols = transposeTable(rows);
-
-    for (const col of cols) {
-      colCtx = drawCol(colCtx, ctx, col);
-    }
-
-    colCtx = {
-      ...colCtx,
-      lineDrawCtx: carriageReturn(colCtx.lineDrawCtx),
-    };
-
-    // drawCursor(
-    //   colCtx.lineDrawCtx.x,
-    //   colCtx.lineDrawCtx.y,
-    //   ctx,
-    //   15 * dpi,
-    //   20 * dpi,
-    //   "#111"
-    // );
-
-    console.log("[dbg] paint.");
-    const maxR = colCtx.lineDrawCtx.maxRight ?? 0;
-    console.log("[dbg] maxRight:", maxR);
-    const maxH = colCtx.lineDrawCtx.maxHeight ?? 0;
-    console.log("[dbg] maxHeight:", maxH);
-
-    setMaxW(maxR + 2 * deltaX);
-    setMaxH(maxH + 2 * deltaY);
-  });
-
-  const [showDbg, setShowDbg] = useState(false);
-
-  return (
-    <Fragment>
-      <Box
-        ref={boxRef}
-        sx={{
-          height: "400px",
-          overflow: "auto",
-        }}
-      >
-        <canvas
-          ref={props.canvasRef}
-          style={{ width: "100%", height: "auto" }}
-          width={R}
-          height={H}
-        ></canvas>
-      </Box>
-      {showDbg && (
-        <Box
-          sx={{
-            paddingTop: 2,
-            paddingLeft: 3,
-            paddingRight: 3,
-            display: "flex",
-            gap: 2,
-            flexWrap: "wrap",
-          }}
-        >
-          <Box>W: {w}</Box>
-          <Box>H: {h}</Box>
-          <Box>MaxW: {maxW.toFixed(0)}</Box>
-          <Box>MaxH: {maxH.toFixed(0)}</Box>
-        </Box>
-      )}
-    </Fragment>
-  );
-}
-
-export default function Page() {
-  const [show, setShow] = useState(true);
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+  }, []);
 
   return (
     <Box>
@@ -596,7 +373,11 @@ export default function Page() {
         <DialogContent
           sx={{ paddingLeft: 0, paddingRight: 0, paddingBottom: 0 }}
         >
-          <Window canvasRef={canvasRef as any} />
+          <CanvasTable
+            preamble={preamble}
+            tabularData={tabularData}
+            canvasRef={canvasRef as any}
+          />
         </DialogContent>
       </Dialog>
     </Box>
---
+++ web/components/canvastable.tsx
@@ -0,0 +1,328 @@
+"use client";
+
+import { Box } from "@mui/material";
+import { Fragment, useEffect, useRef, useState } from "react";
+
+type LineDrawCtx = {
+  fillStyle?: CanvasPattern | string | CanvasGradient;
+  baseline?: CanvasTextBaseline;
+  font?: string;
+  y: number;
+  lineGap: number;
+  lastMetrics?: TextMetrics;
+  x: number;
+  maxWidth?: number;
+  maxRight?: number;
+  maxHeight?: number;
+};
+
+function drawLine(
+  lineCtx: LineDrawCtx,
+  ctx: CanvasRenderingContext2D,
+  line: string
+): LineDrawCtx {
+  if (lineCtx.baseline) {
+    ctx.textBaseline = lineCtx.baseline;
+  }
+  if (lineCtx.fillStyle) {
+    ctx.fillStyle = lineCtx.fillStyle;
+  }
+  if (lineCtx.font) {
+    ctx.font = lineCtx.font;
+  }
+  ctx.fillText(line, lineCtx.x, lineCtx.y);
+  const lineMs = ctx.measureText(line);
+  const prevMaxWidth = lineCtx.maxWidth ?? 0;
+  const maxWidth = Math.max(prevMaxWidth, lineMs.width);
+  const prevMaxRight = lineCtx.maxRight ?? 0;
+  const maxRight = Math.max(prevMaxRight, lineCtx.x + lineMs.width);
+  const prevMaxHeight = lineCtx.maxHeight ?? 0;
+  const newHeight =
+    lineCtx.y +
+    lineMs.fontBoundingBoxAscent +
+    lineMs.fontBoundingBoxDescent +
+    lineCtx.lineGap;
+  const maxHeight = Math.max(prevMaxHeight, newHeight);
+
+  return {
+    ...lineCtx,
+    y: newHeight,
+    lastMetrics: lineMs,
+    maxWidth,
+    maxRight,
+    maxHeight,
+  };
+}
+
+function drawCursor(
+  x: number,
+  y: number,
+  ctx: CanvasRenderingContext2D,
+  w: number,
+  h: number,
+  color: CanvasPattern | string | CanvasGradient
+): void {
+  const currentFill = ctx.fillStyle;
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, w, h);
+  ctx.fillStyle = currentFill;
+}
+
+type ColDrawCtx = {
+  lineDrawCtx: LineDrawCtx;
+  columnGap: number;
+  y0?: number;
+};
+
+export type Cell = {
+  content: string;
+  empty?: boolean;
+};
+
+export type Col = Cell[];
+
+export type Row = Cell[];
+
+function transposeTable(rows: Row[]): Col[] {
+  if (rows.length === 0) {
+    return [];
+  }
+  const nCols = rows[0].length;
+  const cols: Col[] = [];
+  for (let c = 0; c < nCols; c++) {
+    const col: Col = [];
+    for (let r = 0; r < rows.length; r++) {
+      if (rows[r].length >= c + 1) {
+        col.push(rows[r][c]);
+      } else {
+        col.push({ content: "", empty: true });
+      }
+    }
+    cols.push(col);
+  }
+  return cols;
+}
+
+function drawCol(
+  colCtx: ColDrawCtx,
+  ctx: CanvasRenderingContext2D,
+  column: Col
+): ColDrawCtx {
+  let newColCtx: ColDrawCtx = {
+    ...colCtx,
+    y0: colCtx.y0 ?? colCtx.lineDrawCtx.y,
+  };
+  newColCtx.lineDrawCtx = {
+    ...newColCtx.lineDrawCtx,
+    maxWidth: 0,
+    y: newColCtx.y0!,
+  };
+
+  for (const cell of column) {
+    newColCtx.lineDrawCtx = drawLine(newColCtx.lineDrawCtx, ctx, cell.content);
+  }
+
+  newColCtx.lineDrawCtx = {
+    ...newColCtx.lineDrawCtx,
+    x:
+      newColCtx.lineDrawCtx.x +
+      (newColCtx.lineDrawCtx.maxWidth ?? 0) +
+      newColCtx.columnGap,
+    maxWidth: 0,
+  };
+
+  return newColCtx;
+}
+
+function carriageReturn(lineCtx: LineDrawCtx): LineDrawCtx {
+  return {
+    ...lineCtx,
+    x: 0,
+  };
+}
+
+export function CanvasTable(props: {
+  preamble: Row[];
+  tabularData: Row[];
+  canvasRef: React.RefObject<HTMLCanvasElement>;
+  showCursor?: boolean;
+  showDbg?: boolean;
+}) {
+  const { preamble, tabularData, showCursor, showDbg } = props;
+  const [w, setW] = useState(0);
+  const [h, setH] = useState(0);
+  const [maxW, setMaxW] = useState(0);
+  const [maxH, setMaxH] = useState(0);
+
+  const dpi = window.devicePixelRatio;
+  const R = Math.max(w * dpi, maxW);
+  const H = Math.max(h * dpi, maxH);
+
+  const boxRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const ele = boxRef.current;
+    if (!ele) {
+      return;
+    }
+
+    const rect = ele.getBoundingClientRect();
+    console.log("[dbg] rect:", rect);
+
+    const obs = new ResizeObserver((entries) => {
+      for (const ent of entries) {
+        if (ent.target === ele) {
+          console.log("[dbg] ResizeObserver entry:", ent);
+          const cr = ent.contentRect;
+          if (cr) {
+            setW(cr.width);
+            setH(cr.height);
+          }
+        }
+      }
+    });
+
+    obs.observe(ele);
+
+    return () => {
+      obs.unobserve(ele);
+    };
+  });
+
+  useEffect(() => {
+    const ele = boxRef.current;
+    if (!ele) {
+      return;
+    }
+
+    const canvasEle = ele.querySelector("canvas");
+    if (!canvasEle) {
+      return;
+    }
+
+    const dpi = window.devicePixelRatio;
+
+    // canvasEle.setAttribute("width", `${w * dpi}`);
+    // canvasEle.setAttribute("height", `${h * dpi}`);
+
+    const ctx = canvasEle.getContext("2d");
+    if (!ctx) {
+      return;
+    }
+
+    ctx.fillStyle = "#262626";
+    ctx.fillRect(0, 0, R, H);
+
+    const deltaX = 10 * dpi;
+    const deltaY = 10 * dpi;
+    const translateMatrix = [
+      [1, 0, deltaX],
+      [0, 1, deltaY],
+      [0, 0, 1],
+    ];
+    ctx.setTransform(
+      translateMatrix[0][0],
+      translateMatrix[1][0],
+      translateMatrix[0][1],
+      translateMatrix[1][1],
+      translateMatrix[0][2],
+      translateMatrix[1][2]
+    );
+    let lineCtx: LineDrawCtx = {
+      fillStyle: "#fff",
+      baseline: "top",
+      font: `${16 * dpi}px sans-serif`,
+      y: 0,
+      lineGap: 6 * dpi,
+      x: 0,
+    };
+
+    for (const row of preamble) {
+      if (row.length > 0) {
+        lineCtx = drawLine(lineCtx, ctx, row[0].content);
+      }
+    }
+
+    // drawCursor(lineCtx, ctx, 15 * dpi, 20 * dpi, "#111");
+
+    let colCtx: ColDrawCtx = {
+      lineDrawCtx: lineCtx,
+      columnGap: 30 * dpi,
+    };
+
+    const cols = transposeTable(tabularData);
+
+    for (const col of cols) {
+      colCtx = drawCol(colCtx, ctx, col);
+    }
+
+    colCtx = {
+      ...colCtx,
+      lineDrawCtx: carriageReturn(colCtx.lineDrawCtx),
+    };
+
+    if (!!showCursor) {
+      drawCursor(
+        colCtx.lineDrawCtx.x,
+        colCtx.lineDrawCtx.y,
+        ctx,
+        15 * dpi,
+        20 * dpi,
+        "#111"
+      );
+    }
+
+    console.log("[dbg] paint.");
+    const maxR = colCtx.lineDrawCtx.maxRight ?? 0;
+    console.log("[dbg] maxRight:", maxR);
+    const maxH = colCtx.lineDrawCtx.maxHeight ?? 0;
+    console.log("[dbg] maxHeight:", maxH);
+
+    const realMaxW = maxR + 2 * deltaX;
+    setMaxW(realMaxW);
+    const realMaxH = maxH + 2 * deltaY;
+    setMaxH(realMaxH);
+
+    return () => {
+      console.log("[dbg] clean up");
+      ctx.resetTransform();
+      ctx.clearRect(0, 0, realMaxW, realMaxH);
+    };
+  });
+
+  return (
+    <Fragment>
+      <Box
+        ref={boxRef}
+        sx={{
+          height: "400px",
+          overflow: "auto",
+        }}
+      >
+        <canvas
+          ref={props.canvasRef}
+          style={{ width: "100%", height: "auto" }}
+          width={R}
+          height={H}
+        ></canvas>
+      </Box>
+      {showDbg && (
+        <Box
+          sx={{
+            paddingTop: 2,
+            paddingLeft: 3,
+            paddingRight: 3,
+            display: "flex",
+            gap: 2,
+            flexWrap: "wrap",
+          }}
+        >
+          <Box>W: {w}</Box>
+          <Box>H: {h}</Box>
+          <Box>MaxW: {maxW.toFixed(0)}</Box>
+          <Box>MaxH: {maxH.toFixed(0)}</Box>
+        </Box>
+      )}
+    </Fragment>
+  );
+}
