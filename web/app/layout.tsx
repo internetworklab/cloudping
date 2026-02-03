37fa0a5f7b8776249889a1cf4fc89b7230a9abf5
duststars <i@exploro.one>
2026-02-03 21:59:46 +0800

    to support darkmode

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -2,8 +2,21 @@
 
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { CssBaseline, createTheme, ThemeProvider } from "@mui/material";
+import {
+  CssBaseline,
+  createTheme,
+  ThemeProvider,
+  useColorScheme,
+  Box,
+  Tooltip,
+  IconButton,
+} from "@mui/material";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Fragment } from "react";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import ContrastIcon from "@mui/icons-material/Contrast";
+import { firstLetterCap } from "@/components/strings";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -54,12 +67,52 @@ const theme = createTheme({
   },
 });
 
+const modes = ["system", "dark", "light"];
+
+function ModeSelector() {
+  const { mode, setMode } = useColorScheme();
+  if (!mode) {
+    return null;
+  }
+  return (
+    <Fragment>
+      <Box sx={{ position: "absolute", top: 0, right: 0 }}>
+        <Box sx={{ padding: 1 }}>
+          <Tooltip title={`Mode: ${firstLetterCap(mode)}`}>
+            <IconButton
+              onClick={() => {
+                const idx = modes.findIndex((m) => m === mode);
+                if (idx !== -1 && modes.length >= 1) {
+                  const nextIdx = (idx + 1) % modes.length;
+                  const nextMode = modes[nextIdx];
+                  setMode(nextMode as any);
+                }
+              }}
+            >
+              {mode === "light" ? (
+                <LightModeIcon />
+              ) : mode === "dark" ? (
+                <DarkModeIcon />
+              ) : mode === "system" ? (
+                <ContrastIcon />
+              ) : (
+                <ContrastIcon />
+              )}
+            </IconButton>
+          </Tooltip>
+        </Box>
+      </Box>
+    </Fragment>
+  );
+}
+
 function WithMUITheme(props: { children: React.ReactNode }) {
   return (
     <ThemeProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
         <CssBaseline />
         {props.children}
+        <ModeSelector />
       </QueryClientProvider>
     </ThemeProvider>
   );
