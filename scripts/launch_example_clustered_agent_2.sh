7995ca06b79fb60bf926ff5919fa5e100916ef3c
root <root@wien1.exploro.one>
2026-02-08 18:31:36 +0000

    updated example launch script

---
+++ scripts/launch_example_clustered_agent_2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+script_path=$(realpath $0)
+script_dir=$(dirname $script_path)
+
+cd $script_dir/..
+
+bin/globalping agent \
+  --quic-server-address="globalping-hub.exploro.one:18449" \
+  --node-name="lax2" \
+  --exact-location-lat-lon="48.1952,16.3503" \
+  --country-code="US" \
+  --city-name="Los Angeles" \
+  --asn="AS35916" \
+  --isp="MULTACOM" \
+  --dn42-asn="AS4242421771" \
+  --dn42-isp="DUSTSTARS"
