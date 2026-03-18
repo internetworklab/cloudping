58ec6d9f2804fc08aa127f01baa61778d8428856
duststars <i@exploro.one>
2026-03-19 02:07:55 +0800

    working on events stream

--- web/app/demos/demo6/page.tsx
+++ web/app/demos/demo6/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Box } from "@mui/material";
+import { Box, Paper, Typography } from "@mui/material";
 import { useEffect, useRef, useState } from "react";
 
 export interface EventObject {
@@ -43,9 +43,12 @@ function EventDock(props: {
   }, [eventsReader]);
 
   return (
-    <Box>
+    <Box sx={{ padding: 1, display: "flex", flexDirection: "column", gap: 2 }}>
       {evs.map((ev) => (
-        <Box key={ev.id}>{ev.message}</Box>
+        <Paper sx={{ padding: 1, borderRadius: 4 }} key={ev.id}>
+          <Typography variant="caption">{`${new Date(ev.timestamp).toISOString()}`}</Typography>
+          <Box>{ev.message}</Box>
+        </Paper>
       ))}
     </Box>
   );
@@ -53,8 +56,76 @@ function EventDock(props: {
 
 function arrayToStream(
   evs: EventObject[],
-): ReadableStreamDefaultReader<EventObject> {}
+): ReadableStreamDefaultReader<EventObject> {
+  let index = 0;
+  let ticker: ReturnType<typeof setInterval>;
+  const stream = new ReadableStream<EventObject>({
+    start(controller) {
+      console.log("[dbg] ev ticker started");
+      ticker = setInterval(() => {
+        console.log("[dbg] ev ticker ticked");
+        if (index < evs.length) {
+          controller.enqueue(evs[index]);
+          index++;
+        } else {
+          clearInterval(ticker);
+          controller.close();
+        }
+      }, 1000);
+    },
+    cancel() {
+      if (ticker) {
+        console.log("[dbg] ev ticker cancelled");
+        clearInterval(ticker);
+      }
+    },
+  });
+  return stream.getReader();
+}
+
+const mockEVs: EventObject[] = [
+  { id: "1", timestamp: Date.now(), message: "System started" },
+  { id: "2", timestamp: Date.now() + 1000, message: "Connection established" },
+  { id: "3", timestamp: Date.now() + 2000, message: "Data sync in progress" },
+  { id: "4", timestamp: Date.now() + 3000, message: "User logged in" },
+  { id: "5", timestamp: Date.now() + 4000, message: "Cache cleared" },
+  { id: "6", timestamp: Date.now() + 5000, message: "Backup completed" },
+  { id: "7", timestamp: Date.now() + 6000, message: "System idle" },
+];
 
 export default function Page() {
-  return <Box></Box>;
+  const [readers, setReaders] = useState<
+    ReadableStreamDefaultReader<EventObject>[]
+  >([]);
+  const readerRef = useRef<ReadableStreamDefaultReader<EventObject>>(undefined);
+  useEffect(() => {
+    if (readerRef.current) {
+      return;
+    }
+
+    const it = setTimeout(() => {
+      const streamReader = arrayToStream(mockEVs);
+      readerRef.current = streamReader;
+      setReaders([streamReader]);
+    });
+
+    return () => {
+      clearTimeout(it);
+      setReaders([]);
+      if (readerRef.current) {
+        const reader = readerRef.current;
+        readerRef.current = undefined;
+        reader
+          .cancel()
+          .catch((e) => console.error("failed to cancel stream:", e));
+      }
+    };
+  }, []);
+  return (
+    <Box>
+      {readers.map((reader, i) => (
+        <EventDock key={i} eventsReader={reader} />
+      ))}
+    </Box>
+  );
 }
